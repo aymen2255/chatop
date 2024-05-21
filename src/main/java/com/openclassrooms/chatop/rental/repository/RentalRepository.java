10778b1cbefd077323c44c074bd90243241da1b6
aymen <aymen2255@hotmail.fr>
2024-05-21 22:59:17 +0200

    clean code

--- src/main/java/com/openclassrooms/chatop/rental/repository/RentalRepository.java
+++ src/main/java/com/openclassrooms/chatop/rental/repository/RentalRepository.java
@@ -1,9 +1,6 @@
 package com.openclassrooms.chatop.rental.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.openclassrooms.chatop.rental.entity.Rental;
