e29b062377c9f77db9d45616871fba5a64501981
aymen <aymen2255@hotmail.fr>
2024-05-30 01:22:09 +0200

    refacto security

--- src/main/java/com/openclassrooms/chatop/auth/service/JWTService.java
+++ src/main/java/com/openclassrooms/chatop/auth/service/JWTService.java
@@ -1,67 +1,65 @@
 package com.openclassrooms.chatop.auth.service;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import java.util.Date;
 import java.util.function.Function;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import com.openclassrooms.chatop.user.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JWTService {
 
-    private SecretKey Key;
-    private  static  final long EXPIRATION_TIME = 86400000;
-    
-    public JWTService(){
-        String secreteString = "843567893696976453275974432697R634976R738467TR678T34865R6834R8763T478378637664538745673865783678548735687R3";
-        byte[] keyBytes = Base64.getDecoder().decode(secreteString.getBytes(StandardCharsets.UTF_8));
-        this.Key = new SecretKeySpec(keyBytes, "HmacSHA256");
-    }
+	@Value("${jwt_expiration}")
+	private long jwtExpiration;
 
-    public String generateToken(User user){
-        return Jwts.builder()
-                .subject(user.getUsername())
-                .claim("userId", user.getId())
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(Key)
-                .compact();
-    }
-    
-    public Integer extractId(String token){
-        return extractClaims(token, claims -> claims.get("userId", Integer.class));
-    }
+	@Value("${secretkey}")
+	private String secreteString;
 
-    public String extractUsername(String token){
-        return extractClaims(token, Claims::getSubject);
-    }
-    
-    private <T> T extractClaims(String token, Function<Claims, T> claimsFunction){
-        return claimsFunction.apply(Jwts.parser().verifyWith(Key).build().parseSignedClaims(token).getPayload());
-    }
+	public String generateToken(User user) {
+		return Jwts.builder().subject(user.getUsername()).claim("userId", user.getId())
+				.issuedAt(new Date(System.currentTimeMillis()))
+				.expiration(new Date(System.currentTimeMillis() + jwtExpiration)).signWith(getSignInKey()).compact();
+	}
 
-    public boolean isTokenValid(String token, UserDetails userDetails){
-        final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-    }
-    
-    public boolean isTokenExpired(String token){
-        return extractClaims(token, Claims::getExpiration).before(new Date());
-    }
-    
-    public String getTokenFromAuthorizationHeader(String authorization) {
-    	
-        if (authorization == null || authorization.isBlank() || !authorization.startsWith("Bearer ")) {
-            throw new IllegalArgumentException("Le format de l'en-tête d'autorisation est incorrect");
-        }
+	public Integer extractId(String token) {
+		return extractClaims(token, claims -> claims.get("userId", Integer.class));
+	}
 
-        return authorization.substring(7);
-    }
-    
+	public String extractUsername(String token) {
+		return extractClaims(token, Claims::getSubject);
+	}
+
+	private <T> T extractClaims(String token, Function<Claims, T> claimsFunction) {
+		return claimsFunction
+				.apply(Jwts.parser().verifyWith(getSignInKey()).build().parseSignedClaims(token).getPayload());
+	}
+
+	public boolean isTokenValid(String token, UserDetails userDetails) {
+		final String username = extractUsername(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+
+	public boolean isTokenExpired(String token) {
+		return extractClaims(token, Claims::getExpiration).before(new Date());
+	}
+
+	public String getTokenFromAuthorizationHeader(String authorization) {
+
+		if (authorization == null || authorization.isBlank() || !authorization.startsWith("Bearer ")) {
+			throw new IllegalArgumentException("Le format de l'en-tête d'autorisation est incorrect");
+		}
+
+		return authorization.substring(7);
+	}
+
+	private SecretKey getSignInKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(secreteString);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
 }
