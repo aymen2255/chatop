2995f4a66fce87229b15973a4297d40b2fddf89e
aymen <aymen2255@hotmail.fr>
2024-05-29 19:30:04 +0200

    change variables dtos name

--- src/main/java/com/openclassrooms/chatop/rental/dto/RentalDTO.java
+++ src/main/java/com/openclassrooms/chatop/rental/dto/RentalDTO.java
@@ -2,6 +2,7 @@ package com.openclassrooms.chatop.rental.dto;
 
 import java.sql.Timestamp;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,9 +30,11 @@ public class RentalDTO {
 	private Integer owner_id;
 
 	@JsonFormat(pattern = "yyyy/MM/dd")
+	@JsonProperty(value = "created_at")
 	private Timestamp createdAt;
 
 	@JsonFormat(pattern = "yyyy/MM/dd")
+	@JsonProperty(value = "updated_at")
 	private Timestamp updatedAt;
 	
 	
