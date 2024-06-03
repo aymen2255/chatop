2e86163f063d4e190bc7009fb4c2be573d41c259
aymen <aymen2255@hotmail.fr>
2024-06-03 17:08:45 +0200

    replace AuthentificationServiceImpl by interface

--- src/main/java/com/openclassrooms/chatop/controller/AuthController.java
+++ src/main/java/com/openclassrooms/chatop/controller/AuthController.java
@@ -10,12 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.openclassrooms.chatop.dto.auth.AuthentificationRequest;
 import com.openclassrooms.chatop.dto.auth.AuthentificationResponse;
 import com.openclassrooms.chatop.dto.auth.RegisterRequest;
-import com.openclassrooms.chatop.service.auth.AuthentificationServiceImpl;
-
+import com.openclassrooms.chatop.service.auth.AuthentificationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -26,7 +24,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
-	private final AuthentificationServiceImpl authService;
+	private final AuthentificationService authService;
 
 	@PostMapping("/register")
 	public ResponseEntity<AuthentificationResponse> register(@RequestBody @Valid RegisterRequest registerRequest) {
