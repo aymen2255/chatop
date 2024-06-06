8171aeb31b24eecf6d3f52dbacb29def0f06113c
aymen <aymen2255@hotmail.fr>
2024-06-07 00:08:28 +0200

    delete try catch bloc and ExceptionHandler on MessageController

--- src/main/java/com/openclassrooms/chatop/controller/MessageController.java
+++ src/main/java/com/openclassrooms/chatop/controller/MessageController.java
@@ -1,12 +1,7 @@
 package com.openclassrooms.chatop.controller;
 
-import java.util.HashMap;
 import org.apache.coyote.BadRequestException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +12,6 @@ import com.openclassrooms.chatop.service.message.MessageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -34,34 +28,15 @@ public class MessageController {
 
 	@Operation(description = "Create message")
 	@PostMapping("/messages")
-	public ResponseEntity<String> newMessage(@Valid @RequestBody CreateMessageDTO createMessageDTO) {
+	public ResponseEntity<String> newMessage(@Valid @RequestBody CreateMessageDTO createMessageDTO)
+			throws BadRequestException {
 
-		try {
+		messageService.newMessage(createMessageDTO);
 
-			messageService.newMessage(createMessageDTO);
+		MessageResponseDTO message = MessageResponseDTO.builder().message("Message send with success").build();
 
-			MessageResponseDTO message = MessageResponseDTO.builder().message("Message send with success").build();
-			
-			return ResponseEntity.ok(message.getMessage());
+		return ResponseEntity.ok(message.getMessage());
 
-		} catch (BadRequestException | EntityNotFoundException e) {
-
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-
-		} catch (Exception e) {
-
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
 	}
 
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException exp) {
-		var errors = new HashMap<String, String>();
-		exp.getBindingResult().getAllErrors().forEach(error -> {
-			var fieldName = ((FieldError) error).getField();
-			var errorMessage = error.getDefaultMessage();
-			errors.put(fieldName, errorMessage);
-		});
-		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-	}
 }
